b80474da276e53f1b0b2288746ab2bb84f19cf75
luisPacheco0930 <l.pacheco.0925@gmail.com>
2017-06-25 23:08:31 -0500

    ejemplo de utilizacion del  codigo, conexion con la bd

---
+++ WebApuestasCliente/ApuestaCliente.BussinesLogic/BL_CodigoAleatorio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using ApuestaCliente.Data;
+using ApuestaCliente.Entity;
+
+namespace ApuestaCliente.BussinesLogic
+{
+    public class BL_CodigoAleatorio
+    {
+        public DataTable BL_ValidarCodigoAlearorio(EN_CodigoAleatorio enCodAleatorio)
+        {
+            DataTable dtLista = new DataTable();
+            DA_CodigoAleatorio daCodAleatorio = new DA_CodigoAleatorio();
+            using (ContextoDB dbContexto = ContextoDB.InicializarContexto())
+            {
+                dtLista = daCodAleatorio.DA_ValidarCodigoAleatorio(dbContexto, enCodAleatorio);
+            }
+            return dtLista;
+        }
+
+        //Registrar o Actualizar
+        //public void BL_Registrar(EN_Deporte enDeporte)
+        //{
+        //    DA_Deporte daDeporte = new DA_Deporte();
+        //    using (ContextoDB contexto = ContextoDB.InicializarContexto())
+        //    {
+        //        contexto.IniciarTransaccion();
+        //        daDeporte.DA_RegistrarDeporte(contexto, enDeporte);
+        //        contexto.ConfirmarTransaccion();
+        //    }
+        //}
+    }
+}
---
+++ WebApuestasCliente/ApuestaCliente.Data/DA_CodigoAleatorio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApuestaCliente.Entity;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ApuestaCliente.Data
+{
+    public class DA_CodigoAleatorio
+    {
+        //Para actaulizar o insertar los registros.....
+        //public void DA_RegistrarDeporte(ContextoDB contexto, EN_Deporte enDeporte)
+        //{
+        //    Dictionary<string, object> dicParametros = new Dictionary<string, object>();
+        //    Dictionary<string, object> dicOutPut = new Dictionary<string, object>();
+        //    dicParametros.Add("@descripcion", Convert.ToString(enDeporte.descripcion));
+        //    dicParametros.Add("@usuario", Convert.ToString(enDeporte.UsuarioCreacion));
+        //    contexto.EjecutarTransaccion("SP_InsertarDeporte", dicParametros);
+        //}
+
+        //Para listar datatable o reader...
+
+        public DataTable DA_ValidarCodigoAleatorio(ContextoDB contexto, EN_CodigoAleatorio enCodigo)
+        {
+            DataTable dtLista = new DataTable();
+            Dictionary<string, object> dicParametros = new Dictionary<string, object>();
+            dicParametros.Add("@NroCodAleatorio", Convert.ToString(enCodigo.NroCodigoAleatorio));
+            dtLista = contexto.RetornarDataTable("SP_ValidarAcceso_CodAleatorio", dicParametros);
+            return dtLista;
+        }
+    }
+}
